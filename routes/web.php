02f43c7a68924b5e9f0f1bbd553f7bbb8cb2141f
Hsu Wai Wai Lwin <hwwl4901@gmail.com>
2025-12-14 00:00:55 +0630

    app/Http/Controllers/Test

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/articles', function () {
-    return 'Article List';
-   });
-   Route::get('/articles/detail', function () {
-    return 'Article Detail';
-   });
-   Route::get('/articles/detail/{id}', function ( $id ) {
-    return "Article Detail - $id";
-   });
-   Route::get('/articles/detail', function () {
-    return 'Article Detail';
-   })->name('article.detail');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/articles', function () {
+//     return 'Article List';
+//    });
+//    Route::get('/articles/detail', function () {
+//     return 'Article Detail';
+//    });
+//    Route::get('/articles/detail/{id}', function ( $id ) {
+//     return "Article Detail - $id";
+//    });
+//    Route::get('/articles/detail', function () {
+//     return 'Article Detail';
+//    })->name('article.detail');
 
-   Route::get('/articles/more', function() {
-    return redirect()->route('article.detail');
-   });
+//    Route::get('/articles/more', function() {
+//     return redirect()->route('article.detail');
+//    });
+
+use App\Http\Controllers\TEst\TestController;
+
+   Route::get('/articles', [TestController::class, 'index1']);
+   Route::get('/articles/detail/{id}', [TestController::class, 'detail']);
+   Route::get('/articles/test', [TestController::class, 'test']);
+   Route::get('/articles/order', [TestController::class, 'order']);
+   Route::get('/articles/items', [TestController::class, 'items']);
