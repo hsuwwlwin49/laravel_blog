97da9067d30ee7efe760a2572f6651be301a0c0a
Hsu Wai Wai Lwin <hwwl4901@gmail.com>
2026-01-12 15:30:44 +0630

    Day11 Assignment updated done!

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Create Post
-        </h2>
-    </x-slot>
-
     <div class="py-12 bg-gray-100">
         <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white p-6 rounded-lg shadow">
